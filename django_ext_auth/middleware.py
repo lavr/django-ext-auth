5188a0012f07051ea8f2a5ff20117976f9e26d8e
Sergey Lavrinenko <s@lavr.me>
2017-08-03 01:32:49 +0300

    Fix login_required behavior

--- django_ext_auth/middleware.py
+++ django_ext_auth/middleware.py
@@ -2,15 +2,20 @@ from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.functional import SimpleLazyObject
+from django.core.exceptions import PermissionDenied
 
 from .tokens import get_user_for_sessionid
 from .utils import get_real_ip
+from .exceptions import BaseException
 
 
 def get_user(request):
     assert settings.EXTAUTH_AUTH_URL, "ExtAuthMiddleware requires settings.EXTAUTH_AUTH_URL"
     sessionid = request.COOKIES.get(settings.EXTAUTH_SESSIONID_COOKIE_NAME)
-    user = get_user_for_sessionid(sessionid, user_ip=get_real_ip(request))
+    try:
+        user = get_user_for_sessionid(sessionid, user_ip=get_real_ip(request))
+    except (PermissionDenied, BaseException):
+        user = None
     return user or AnonymousUser()
 
 
